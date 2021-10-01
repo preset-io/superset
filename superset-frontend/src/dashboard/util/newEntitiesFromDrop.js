a59343b8e0497083f9f08ba6d56b0bb2414d8781
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2021-10-01 14:48:38 +0300

    fix(dashboard): don't add recursive parents on wrapper component
    (cherry picked from commit 94e29530c573a7226653f24f6dc5ed5893442910)

--- superset-frontend/src/dashboard/util/newEntitiesFromDrop.js
+++ superset-frontend/src/dashboard/util/newEntitiesFromDrop.js
@@ -53,7 +53,6 @@ export default function newEntitiesFromDrop({ dropResult, layout }) {
     newEntities[rowWrapper.id] = rowWrapper;
     newDropChild.parents = rowWrapper.parents.concat(rowWrapper.id);
     newDropChild = rowWrapper;
-    newDropChild.parents = rowWrapper.parents.concat(rowWrapper.id);
   } else if (dragType === TABS_TYPE) {
     // create a new tab component
     const tabChild = newComponentFactory(TAB_TYPE);
