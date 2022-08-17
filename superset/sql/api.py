0c55161c51604d6dc241706eb0ec6be3f7edcd7c
Eric Briscoe <eric.j.briscoe@gmail.com>
2022-08-16 19:34:53 -0700

    Enables new api/v1/sql endpoint to return JSON
    Two remaining issues here:
    1. need to figure out how to not use class_permission_name = "Explore"
    2. Lots of copy paste code duplication from sql_lab/views.py

--- superset/sql/api.py
+++ superset/sql/api.py
@@ -16,19 +16,22 @@
 # under the License.
 import json
 import logging
+from typing import Any, Callable, cast, Dict, List, Optional, Union
 
 from flask import g, request, Response
 from flask_appbuilder.api import BaseApi, expose, protect, safe
 
-from superset import app
+from superset import app, db, event_logger, is_feature_enabled
 from superset.charts.commands.exceptions import ChartNotFoundError
 from superset.constants import MODEL_API_RW_METHOD_PERMISSION_MAP, RouteMethod
+from superset.databases.dao import DatabaseDAO
 from superset.explore.commands.get import GetExploreCommand
 from superset.explore.commands.parameters import CommandParameters
 from superset.explore.exceptions import DatasetAccessDeniedError, WrongEndpointError
 from superset.explore.permalink.exceptions import ExplorePermalinkGetFailedError
 from superset.explore.schemas import ExploreContextSchema
 from superset.extensions import event_logger
+from superset.models.sql_lab import Query, TabState
 from superset.utils import core as utils
 from superset.utils.core import get_user_id
 from superset.views.base import common_bootstrap_payload
@@ -38,17 +41,80 @@ logger = logging.getLogger(__name__)
 
 config = app.config
 
+DATABASE_KEYS = [
+    "allow_file_upload",
+    "allow_ctas",
+    "allow_cvas",
+    "allow_dml",
+    "allow_multi_schema_metadata_fetch",
+    "allow_run_async",
+    "allows_subquery",
+    "backend",
+    "database_name",
+    "expose_in_sqllab",
+    "force_ctas_schema",
+    "id",
+    "disable_data_preview",
+]
+
 
 class SqlRestApi(BaseApi):
     method_permission_name = MODEL_API_RW_METHOD_PERMISSION_MAP
     include_route_methods = {RouteMethod.GET}
-    # allow_browser_login = True
-    class_permission_name = "Superset"
+    allow_browser_login = True
+    # TO DO: make this use Sql permission not Explore
+    class_permission_name = "Explore"
     resource_name = "sql"
-    # openapi_spec_tag = "Explore"
+    # openapi_spec_tag = "Sql"
     # openapi_spec_component_schemas = (ExploreContextSchema,)
 
+    @staticmethod
+    def _get_sqllab_tabs(user_id: Optional[int]) -> Dict[str, Any]:
+        # send list of tab state ids
+        tabs_state = (
+            db.session.query(TabState.id, TabState.label)
+            .filter_by(user_id=user_id)
+            .all()
+        )
+        tab_state_ids = [str(tab_state[0]) for tab_state in tabs_state]
+        # return first active tab, or fallback to another one if no tab is active
+        active_tab = (
+            db.session.query(TabState)
+            .filter_by(user_id=user_id)
+            .order_by(TabState.active.desc())
+            .first()
+        )
+
+        databases: Dict[int, Any] = {}
+        for database in DatabaseDAO.find_all():
+            databases[database.id] = {
+                k: v for k, v in database.to_json().items() if k in DATABASE_KEYS
+            }
+            databases[database.id]["backend"] = database.backend
+        queries: Dict[str, Any] = {}
+
+        # These are unnecessary if sqllab backend persistence is disabled
+        if is_feature_enabled("SQLLAB_BACKEND_PERSISTENCE"):
+            # return all user queries associated with existing SQL editors
+            user_queries = (
+                db.session.query(Query)
+                .filter_by(user_id=user_id)
+                .filter(Query.sql_editor_id.in_(tab_state_ids))
+                .all()
+            )
+            queries = {
+                query.client_id: dict(query.to_dict().items()) for query in user_queries
+            }
+
+        return {
+            "tab_state_ids": tabs_state,
+            "active_tab": active_tab.to_dict() if active_tab else None,
+            "databases": databases,
+            "queries": queries,
+        }
+
     @expose("/", methods=["GET"])
+    # need to track down where permision is assinged to roles
     @protect()
     @safe
     @event_logger.log_this_with_context(
@@ -56,6 +122,7 @@ class SqlRestApi(BaseApi):
         log_to_statsd=True,
     )
     def get(self) -> Response:
+
         payload = {
             "defaultDbId": config["SQLLAB_DEFAULT_DBID"],
             "common": common_bootstrap_payload(),
