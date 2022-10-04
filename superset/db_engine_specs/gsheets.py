2f4bb0e3a1938d4fc72ff65b824629f363773187
lyndsiWilliams <kcatgirl@gmail.com>
2022-10-04 16:22:38 -0500

    Fix unintentional merge changes

--- superset/db_engine_specs/gsheets.py
+++ superset/db_engine_specs/gsheets.py
@@ -251,6 +251,7 @@ class GSheetsEngineSpec(SqliteEngineSpec):
         conn = engine.connect()
         idx = 0
         for name, url in table_catalog.items():
+
             if not name:
                 errors.append(
                     SupersetError(
