3cbf16a8e094a25ecd960411a06aca5f031565a4
lyndsiWilliams <kcatgirl@gmail.com>
2023-01-03 15:49:55 -0600

    Change all add dataset entrances to use new create page

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
@@ -1231,7 +1231,7 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
         onClick={() => {
           setLoading(true);
           fetchAndSetDB();
-          window.location.href = '/tablemodelview/list#create';
+          window.location.href = '/dataset/add/';
         }}
       >
         {t('CREATE DATASET')}
